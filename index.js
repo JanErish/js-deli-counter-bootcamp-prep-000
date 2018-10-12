7b0c79c90bd864686bb072209d3b7736190f634d
janerishbaluca <janerishbaluca@gmail.com>
2018-10-12 19:42:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ function takeANumber(line,name) {
   return `Welcome, ${name}. You are number ${line.length} in line.`;
 }
 
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return 'There is nobody waiting to be served!'
+  }
+}
