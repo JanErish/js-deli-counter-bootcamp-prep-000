fef23c3bb377b7b6a34aacb137d88de73e87b509
janerishbaluca <janerishbaluca@gmail.com>
2018-10-12 19:21:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function takeANumber(line,name) {
   line.push(name);
-  return `Welcome, ${name}. You are number ${(line.length+1)} in line.`
+  return `Welcome, ${name}. You are number ${(line.length+1)} in line.`;
 }
 
