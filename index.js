2a84b33e6a1bb4d10c4341be3d5285190246ff8a
janerishbaluca <janerishbaluca@gmail.com>
2018-10-12 19:54:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,12 @@ function nowServing(katzDeliLine) {
 
 function currenLine(currentLine) {
   var string = 'The line is currently:';
-  for (let i=0;i<currentLine.length;i++)
+  if (currentLine.length === 0) {
+    return 'The line is currently empty.';
+  } else {
+    for (let i=0;i<currentLine.length;i++) {
+      string + ` ${(i+1)} ${currentLine[i]},`;
+    }
+  }
+  return string;
 }
